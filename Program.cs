b6372ec374b6997b4f50d4335076201bb3e4904f
rainxh11 <rainxh11@gmail.com>
2022-09-30 14:14:30 +0100

    + Streamlined source code, updated to .NET 6, added support for Window 11 & newer Windows 10 builds

--- Program.cs
+++ Program.cs
@@ -1,106 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Diagnostics;
-using System.Threading;
-using System.Globalization;
+﻿using System.Text.Json;
+using SharpCompress.Archives;
+using SharpCompress.Archives.Zip;
+using SharpCompress.Common;
+using SharpCompress.Writers.Zip;
+
+var json = File.ReadAllText("whitelist.json");
+var whiteList = JsonSerializer.Deserialize<List<string>>(json);
+
+var winSxsPath = args.Length > 0
+    ? new DirectoryInfo(args.First())
+    : new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "WinSxS"));
+
+var foldersToCopy = whiteList?
+    .SelectMany(pattern => winSxsPath.EnumerateDirectories($"*{pattern}*", SearchOption.TopDirectoryOnly))
+    .DistinctBy(folder => folder.FullName);
+
+var filesToCopy = foldersToCopy?
+    .SelectMany(folder => folder.EnumerateFiles("*.*", SearchOption.AllDirectories))
+    .DistinctBy(file => file.FullName)
+    .ToList();
 
 
-namespace WinSxS_Cleaner
+using var zipFile = ZipArchive.Create();
+foreach (var folder in filesToCopy)
 {
-    class Program
-    {
-        private static void RetrieveFoldersPath(string dirToSearch, List<string> foldersToSearchFor, List<DirectoryInfo> folderToCopy)
-        {
-            DirectoryInfo dir = new DirectoryInfo(dirToSearch);
-            try
-            {
-                Parallel.ForEach(dir.GetDirectories("*", SearchOption.TopDirectoryOnly), (DirectoryInfo folder) =>
-                //foreach (DirectoryInfo folder in dir.GetDirectories("*", SearchOption.TopDirectoryOnly))
-                {
-                    Console.WriteLine($"Processing {folder.Name} on thread {Thread.CurrentThread.ManagedThreadId}");
-                    foreach (string filter in foldersToSearchFor)
-                    {
-                        if (folder.Name.Contains(filter))
-                        {
-                            folderToCopy.Add(folder);
-                        }
-                    }
-                //}
-                });
-            }
-            catch { }
-        }
-        /// ----------------------------------------------------------------------//
-        static void CopyFolder(string destFolder, DirectoryInfo sourceFolder)
-        {
-            Process robocopyProcess = new Process();
-            robocopyProcess.StartInfo.RedirectStandardOutput = true;
-            robocopyProcess.StartInfo.CreateNoWindow = true;
-            robocopyProcess.StartInfo.UseShellExecute = false;
-            robocopyProcess.StartInfo.Arguments = $@"{sourceFolder.FullName} {destFolder}\{sourceFolder.Name} /E";
-            robocopyProcess.StartInfo.FileName = "robocopy.exe";
-            try
-            {
-                robocopyProcess.Start();
-            }
-            catch { }
-        }
-        /// ----------------------------------------------------------------------//
-        static void Main(string[] args)
-        {
-            string currentDir = AppContext.BaseDirectory;
-			string destFolder = currentDir + @"\Sakonito_WinSxS_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
-
-            List<string> foldersToSearchFor = new List<string>()
-            {
-                "amd64_microsoft-windows-s..cingstack.resources",
-                "amd64_microsoft-windows-servicingstack",
-                "amd64_microsoft.vc80",
-                "amd64_microsoft.vc90",
-                "amd64_microsoft.windows.c..-controls.resources",
-                "amd64_microsoft.windows.c..-controls.resources",
-                "amd64_microsoft.windows.common-controls",
-                "amd64_microsoft.windows.common-controls",
-                "amd64_microsoft.windows.gdiplus",
-                "amd64_microsoft.windows.gdiplus",
-                "Catalogs",
-                "InstallTemp",
-                "Manifests",
-                "x86_microsoft.vc80",
-                "x86_microsoft.vc90",
-                "x86_microsoft.windows.c..-controls.resources",
-                "x86_microsoft.windows.c..-controls.resources",
-                "x86_microsoft.windows.common-controls",
-                "x86_microsoft.windows.common-controls",
-                "x86_microsoft.windows.gdiplus",
-                "x86_microsoft.windows.gdiplus"
-            };
-            List<DirectoryInfo> foldersToCopy = new List<DirectoryInfo>();
-
-            RetrieveFoldersPath(args[0], foldersToSearchFor, foldersToCopy);
-			
-			if (args.Length == 2)
-			{
-				destFolder = args[1];
-			}
-			
-            if (!Directory.Exists(destFolder))
-            {
-				try
-                {
-                    Directory.CreateDirectory(destFolder);
-                }
-                catch {}
-            }
-
-            foreach(DirectoryInfo folder in foldersToCopy)
-            {
-                CopyFolder(destFolder, folder);
-            }
-        }
-    }
+    zipFile.AddEntry(folder.FullName.Replace(winSxsPath.FullName, ""), folder.FullName);
 }
+
+zipFile.SaveTo($"WinSxS_{filesToCopy?.Count}-files_{DateTime.Now:yyyy-MM-dd}.zip", CompressionType.Deflate);
--- README.md
+++ README.md
@@ -7,12 +7,11 @@ Useful for saving disk space, and creatine custom lightweight Windows ISOs.
 - Windows 7 SP1: SUCCESS!
 - Windows 8: SUCCESS!
 - Windows 8.1: SUCCESS!
-- windows 10 v1903 and earlier: SUCCESS!
-- Windows 10 v1909: FAILED! Will add support in upcoming version.
-- Windows 10 v2004: FAILED! Will add support in upcoming version.
+- Windows 10 From v1903 to latest version: SUCCESS!
+- Windows 11 All Build Verions: SUCCESS!
 
 # How to:
-Drag and drop "windows\winsxs" folder to "WinSxS_Cleaner.exe", it will create reduced size modified "winsxs" folder next to the executable.
+Drag and drop "windows\winsxs" folder to "WinSxSCleaner.exe", it will create reduced size modified "WinSxS" zip file in the executable folder.
 
 Download: https://github.com/rainxh11/WinSxS_Cleaner/releases/latest
 
